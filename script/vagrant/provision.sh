15f374c2ccbf4914c92dc5c92162225f2538c3b0
FunTimeCoding <funtimecoding@gmail.com>
2018-07-04 17:49:57 +0200

    install jdk because maven only pulls in jre

--- script/vagrant/provision.sh
+++ script/vagrant/provision.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get --quiet 2 install neovim multitail htop git tree hunspell shellcheck maven
+apt-get --quiet 2 install neovim multitail htop git tree hunspell shellcheck openjdk-8-jdk-headless maven
