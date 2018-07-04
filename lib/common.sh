b939afd04bfae7248e9748d300985bc5ee56d715
FunTimeCoding <funtimecoding@gmail.com>
2018-07-04 16:38:43 +0200

    filter target directory

--- lib/common.sh
+++ lib/common.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -e
 
-# vendor is in here to not break php-skeleton based projects when synchronizing with them.
-EXCLUDE_FILTER='^.*/(build|tmp|vendor|\.git|\.vagrant|\.idea)/.*$'
+EXCLUDE_FILTER='^.*/(build|tmp|target|\.git|\.vagrant|\.idea)/.*$'
 export EXCLUDE_FILTER
