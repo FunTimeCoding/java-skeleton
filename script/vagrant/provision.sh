56366f7314036f3cba61c86edcda184c6364b66e
FunTimeCoding <funtimecoding@gmail.com>
2018-07-04 17:37:54 +0200

    install maven on virtual machine

--- script/vagrant/provision.sh
+++ script/vagrant/provision.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get --quiet 2 install neovim multitail htop git tree hunspell shellcheck
+apt-get --quiet 2 install neovim multitail htop git tree hunspell shellcheck maven
