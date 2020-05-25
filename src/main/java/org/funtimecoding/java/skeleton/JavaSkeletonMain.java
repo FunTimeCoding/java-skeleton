f3a858098a384507c7780620b40ae18ed3bd6b5a
Alexander Reitzel <funtimecoding@gmail.com>
2020-05-25 03:16:20 +0200

    suppress javadoc checkstyle warnings, fix other concerns of checkstyle

--- src/main/java/org/funtimecoding/java/skeleton/JavaSkeletonMain.java
+++ src/main/java/org/funtimecoding/java/skeleton/JavaSkeletonMain.java
@@ -3,16 +3,21 @@ package org.funtimecoding.java.skeleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JavaSkeletonMain {
-    private static final Logger logger = LoggerFactory.getLogger(JavaSkeletonMain.class);
+public final class JavaSkeletonMain {
+    private static final Logger LOGGER = LoggerFactory.getLogger(
+        JavaSkeletonMain.class
+    );
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         JavaSkeletonMain application = new JavaSkeletonMain();
         System.exit(application.run());
     }
 
     int run() {
-        logger.info("Hello friend from {}", JavaSkeletonMain.class.getSimpleName());
+        LOGGER.info(
+            "Hello friend from {}",
+            JavaSkeletonMain.class.getSimpleName()
+        );
 
         return 0;
     }
